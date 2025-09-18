4c5ace5d8d3a5e5ad64994ed800d035b363081bf
Volodymyr Voronov <malones.vladimir@gmail.com>
2025-09-18 19:31:35 +0200

    Add Bangers font to header title

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,19 @@
+import "@fontsource/bangers/400.css";
+
 import ThemeSwitcher from "./ThemeSwitcher";
 
 const Header = () => {
   return (
     <header className="border-b">
       <div className="px-2 flex flex-row items-center justify-between max-w-2xl mx-auto">
-        <h1 className="text-2xl font-bold">Jobs</h1>
+        <h1
+          className="text-2xl font-bold"
+          style={{
+            fontFamily: "Bangers, cursive, system-ui",
+          }}
+        >
+          Jobs
+        </h1>
         <ThemeSwitcher />
       </div>
     </header>

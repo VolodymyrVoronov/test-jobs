f16938ad4c57968384c73ffb504e24e8a102b5c5
Volodymyr Voronov <malones.vladimir@gmail.com>
2025-09-18 19:32:39 +0200

    Simplify Header component structure and update max-width styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,18 +4,17 @@ import ThemeSwitcher from "./ThemeSwitcher";
 
 const Header = () => {
   return (
-    <header className="border-b">
-      <div className="px-2 flex flex-row items-center justify-between max-w-2xl mx-auto">
-        <h1
-          className="text-2xl font-bold"
-          style={{
-            fontFamily: "Bangers, cursive, system-ui",
-          }}
-        >
-          Jobs
-        </h1>
-        <ThemeSwitcher />
-      </div>
+    <header className="border-b px-2 flex flex-row items-center justify-between max-w-full">
+      <h1
+        className="text-2xl font-bold"
+        style={{
+          fontFamily: "Bangers, cursive, system-ui",
+        }}
+      >
+        Jobs
+      </h1>
+
+      <ThemeSwitcher />
     </header>
   );
 };

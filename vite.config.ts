d9b5559201dc112144fe1b5b0b6e17d57fbf7eab
Volodymyr Voronov <malones.vladimir@gmail.com>
2025-09-19 20:39:35 +0200

    updated vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from "path";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/test-jobs/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
